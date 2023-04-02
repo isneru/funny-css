800fdc83945f3b004cd72ab49e95ea7b67daffc6
isneru <diogoclanogueira@gmail.com>
2023-04-02 23:16:30 +0100

    Twitch login verification code

--- src/app/twitch-verification-code/page.tsx
+++ src/app/twitch-verification-code/page.tsx
@@ -1,7 +1,55 @@
+"use client"
+
+import { useState } from "react"
+
+type TCode = (undefined | number)[]
+
 export default function TwitchVerificationCode() {
+  const NUMBER_OF_DIGITS = 6
+  const [code, setCode] = useState<TCode>(new Array(NUMBER_OF_DIGITS).fill(0))
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value, id } = e.target
+
+    if (value.length > 1) {
+      setCode(
+        code.map((digit, index) =>
+          index === Number(id) ? Number(value[value.length - 1]) : digit
+        )
+      )
+
+      focusNextInput(Number(id))
+      return
+    }
+
+    setCode(
+      code.map((digit, index) => (index === Number(id) ? Number(value) : digit))
+    )
+
+    focusNextInput(Number(id))
+  }
+
+  function focusNextInput(id: number) {
+    const nextInput = document.getElementById(`${id + 1}`)
+    if (nextInput) {
+      nextInput.focus()
+    }
+  }
+
   return (
     <div className="container mx-auto h-[400px] rounded bg-zinc-900">
-      <h1>Twitch Verification Code</h1>
+      <div className="flex h-full items-center justify-center gap-5">
+        {code.map((digit, idx) => (
+          <input
+            key={idx}
+            className="h-20 w-20 rounded bg-zinc-800 text-center text-zinc-100 caret-transparent outline-none focus:border-2 focus:border-white/20"
+            type="number"
+            value={!!digit ? digit : ""}
+            id={idx.toString()}
+            onChange={handleInputChange}
+          />
+        ))}
+      </div>
     </div>
   )
 }
